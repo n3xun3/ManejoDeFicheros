6b662ad013f08ce616e9108df615aa2fc40e0c69
Jorge Ruiz Martínez <jorgetafd@gmail.com>
2024-01-29 17:46:40 +0100

    JavaDoc Main

--- src/main/java/manejoFicheros/Main.java
+++ src/main/java/manejoFicheros/Main.java
@@ -1,10 +1,17 @@
 package manejoFicheros;
 
+/**
+ * La clase Main es la clase principal que contiene el método `main` para iniciar la aplicación.
+ */
 public class Main {
+    /**
+     * Método principal que inicia la aplicación.
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
-        Vista vista = new Vista();
-        Modelo modelo = new Modelo();
-        Controlador controlador = new Controlador(vista, modelo);
-        controlador.iniciar();
+        Vista vista = new Vista(); // Instancia de la vista
+        Modelo modelo = new Modelo(); // Instancia del modelo
+        Controlador controlador = new Controlador(vista, modelo); // Instancia del controlador
+        controlador.iniciar(); // Iniciar la aplicación
     }
 }
